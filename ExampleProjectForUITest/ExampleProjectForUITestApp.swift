3a57271c6157c9254fa2d173e3e3da9fb20ed8e9
Saran Nonkamjan <saran.non@gmail.com>
2023-02-23 09:27:01 +0700

    Initial Commit

---
+++ ExampleProjectForUITest/ExampleProjectForUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleProjectForUITestApp.swift
+//  ExampleProjectForUITest
+//
+//  Created by Saran Nonkamjan on 23/2/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ExampleProjectForUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
